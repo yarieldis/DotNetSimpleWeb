5944e3d5c6198b55357f6f5cc7419d874b79942c
yarieldis <yarieldis@gmail.com>
2018-10-16 17:55:36 -0400

    Testing the template rendering

--- WebApp/Pages/Index.cshtml.cs
+++ WebApp/Pages/Index.cshtml.cs
@@ -4,14 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.Email;
+using WebApp.Models;
+using WebApp.Razor;
 
 namespace WebApp.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly IEmailService _emailService;
+        private readonly IRazorRenderService _renderService;
 
+        public IndexModel(IEmailService emailService, IRazorRenderService renderService)
+        {
+            _emailService = emailService;
+            _renderService = renderService;
+        }
+        public async Task OnGet()
+        {
+            var body = await _renderService.RenderToStringAsync("HelloView", new UserModel
+            {
+                Id = 1,
+                Name = "Hello"
+            });
+            await _emailService.SendEmailAsync("", "", body);
         }
     }
 }
